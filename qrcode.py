a93209e1071918c2db1acdd8974242f0bb049903
Ahmet GENÇ <109852722+gencahmet@users.noreply.github.com>
2022-12-22 00:54:06 +0300

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -16,26 +16,32 @@ vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
 
 while True:
-frame = vs.read()
-frame = imutils.resize(frame, width=400)
-barcodes = pyzbar.decode(frame)
-for barcode in barcodes:
-(x, y, w, h) = barcode.rect
-cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-barcodeData = barcode.data.decode("utf-8")
-barcodeType = barcode.type
-text = "{} ({})".format(barcodeData, barcodeType)
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+    barcodes = pyzbar.decode(frame)
+    for barcode in barcodes:
+        (x, y, w, h) = barcode.rect
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        barcodeData = barcode.data.decode("utf-8")
+        barcodeType = barcode.type
+        text = "{} ({})".format(barcodeData, barcodeType)
         print (text)
-cv2.putText(frame, text, (x, y - 10),
-cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.putText(frame, text, (x, y - 10),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-# if the barcode text is currently not in our CSV file, write
-# the timestamp + barcode to disk and update the set
-if barcodeData not in found:
-csv.write("{},{}\n".format(datetime.datetime.now(),
-barcodeData))
-csv.flush()
-found.add(barcodeData)
-cv2.imshow("Barcode Reader", frame)
-key = cv2.waitKey(1) & 0xFF
-    
+        # if the barcode text is currently not in our CSV file, write
+        # the timestamp + barcode to disk and update the set
+        if barcodeData not in found:
+            csv.write("{},{}\n".format(datetime.datetime.now(),barcodeData))
+            csv.flush()
+            found.add(barcodeData)
+            cv2.imshow("Barcode Reader", frame)
+            key = cv2.waitKey(1) & 0xFF
+        
+         # if the `s` key is pressed, break from the loop
+        if key  == ord("s"):
+            break
+        print("[INFO] cleaning up...")
+        csv.close()
+        cv2.destroyAllWindows()
+        vs.stop()
